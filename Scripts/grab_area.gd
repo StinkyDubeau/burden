39d263cd55b9d19a2e701df86028015549d2a5e0
Jonah_Switzer <jonahswitzeremail@gmail.com>
2024-11-18 16:09:34 -0500

    Started adding ability to grab bags

---
+++ Scripts/grab_area.gd
@@ -0,0 +1,8 @@
+extends Area3D
+
+@onready var parent = get_node("../")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not parent == null:
+		global_transform.origin = parent.get_point_transform(0)
